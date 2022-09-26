3e91e9942a27b606e2fd5bf811ab7852db4482b4
Patrick <74751873+PatrickCuentas@users.noreply.github.com>
2022-09-25 23:17:32 -0500

    Base Template

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from "@angular/material/input"
+import { MatIconModule } from "@angular/material/icon"
+import { MatToolbarModule } from "@angular/material/toolbar"
+
+@NgModule({
+	declarations: [
+		AppComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatTableModule,
+		MatPaginatorModule,
+		MatInputModule,
+		MatButtonModule,
+		MatFormFieldModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatSortModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
